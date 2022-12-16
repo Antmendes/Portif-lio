43c98e901e5d1143b19a8752c0b2a9f83513563b
Antmendes <97694520+Antmendes@users.noreply.github.com>
2022-12-16 11:50:46 -0300

    dinamizando o restante das propriedades

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,8 +32,50 @@ function updateSoftSkills(profileData){
 function updateHardSkills(profileData){
     const hardSkills = document.getElementById('profile.skills.hardSkills')
     hardSkills.innerHTML = profileData.skills.hardSkills.map(skill => `<li><img src="${skill.logo}" alt="${skill.name}" title="${skill.name}"></li>`).join('')
-    
+}
 
+function updateLanguages(profileData){
+    const languages = document.getElementById('profile.languages')
+    languages.innerHTML = profileData.language.map(language => `<li>${language}</li>`).join('')
+}
+
+function updatePortfolio(profileData){
+    const portfolio = document.getElementById('profile.portfolio')
+    portfolio.innerHTML = profileData.portfolio.map(project =>{
+        return `
+        <li>
+        <h3 class="title ">${project.name}</h3>
+        <p>${project.description}</p>
+       <div class="link">
+        <a href="${project.githubUrl}" target="_blank"><i class="fa-brands fa-github"></i>GitHub</a>
+        <a href="${project.siteUrl}" target="_blank"><i class="fa-solid fa-link"></i>Site</a>
+       </div>
+    </li>`
+    }).join('')
+}
+
+function updateFormacao(profileData){
+    const formacao = document.getElementById('profile.education')
+    formacao.innerHTML = profileData.education.map(info => {
+        return `
+        <li>
+                <h3 class="title">${info.school}</h3>
+                <h5>${info.curso}</h5>
+                <p class="period">${info.period}</p>
+              </li>`
+    }).join('')
+}
+
+function updateCursos(profileData){
+    const cursos = document.getElementById('profile.curso')
+    cursos.innerHTML = profileData.cursos.map(curso => {
+        return `
+        <li>
+                <h3 class="title">${curso.name}</h3>
+                <h5>${curso.description}</h5>
+                <p class="period">${curso.period}</p>
+              </li>`
+    }).join('')
 }
 
 (async () =>{
@@ -41,4 +83,8 @@ function updateHardSkills(profileData){
     updateProfileInfo(profileData)
     updateSoftSkills(profileData)
     updateHardSkills(profileData)
+    updateLanguages(profileData)
+    updatePortfolio(profileData)
+    updateFormacao(profileData)
+    updateCursos(profileData)
 })()
